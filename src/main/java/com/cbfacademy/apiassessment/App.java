71779a4a8426da849f6af256423e40bbbb517f9e
Bariyat Eyiowuawi <taiwoeyiowuawi@gmail.com>
2023-10-14 17:25:40 +0100

    unable to get dummy data to work with cass files, may have to start again

--- src/main/java/com/cbfacademy/apiassessment/App.java
+++ src/main/java/com/cbfacademy/apiassessment/App.java
@@ -1,21 +1,16 @@
 package com.cbfacademy.apiassessment;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("/books")
 public class App {
-
-	// @Autowired
-	// private BooksService booksService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
@@ -26,11 +21,9 @@ public class App {
 		return String.format("Hello %s", name);
 	}
 
-    // @GetMapping("all")
-    // public ResponseEntity<List<Books>> getAllBooks(){
-    //     List<Books> books = booksService.getAllBooks();
-    //     return ResponseEntity.ok(books);
-    // }
 }
+
+
+
 
 
