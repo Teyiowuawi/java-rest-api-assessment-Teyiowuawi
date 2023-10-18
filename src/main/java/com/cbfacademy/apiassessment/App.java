cc2bcd76b3e90238b6af03e6f8671262cd46ec58
Bariyat Eyiowuawi <taiwoeyiowuawi@gmail.com>
2023-10-18 08:12:28 +0100

    merged changes to main on local

--- src/main/java/com/cbfacademy/apiassessment/App.java
+++ src/main/java/com/cbfacademy/apiassessment/App.java
@@ -1,10 +1,6 @@
 package com.cbfacademy.apiassessment;
 
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,9 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-
 @SpringBootApplication
 @RestController
 @RequestMapping("books")
@@ -27,7 +20,6 @@ public class App {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
-
 
 	@Autowired
 	private BooksService booksService;
