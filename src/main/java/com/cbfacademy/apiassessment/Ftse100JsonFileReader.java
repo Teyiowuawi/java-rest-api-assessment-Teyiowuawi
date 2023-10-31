cff11dfb893719e2bb59cec063a0722993859c6f
Bariyat Eyiowuawi <taiwoeyiowuawi@gmail.com>
2023-10-31 10:18:45 +0000

    Implementation of json file reader in new class

--- src/main/java/com/cbfacademy/apiassessment/Ftse100JsonFileReader.java
+++ src/main/java/com/cbfacademy/apiassessment/Ftse100JsonFileReader.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import com.google.gson.Gson;
@@ -11,22 +12,15 @@ import com.google.gson.reflect.TypeToken;
 
 public class Ftse100JsonFileReader {
     
-    public JsonFileReader(){}
-
-    List<Ftse100> companies = new ArrayList<>();
-
-    public List<Ftse100> convertJsonToPojo(){
-       
+    public static List<Ftse100> readFtse100JsonFile(String jsonFile){
         Gson gson = new Gson();
 			
-		try (Reader reader = new InputStreamReader(getClass().getResourceAsStream("ftse100.json"))){
-			companies = gson.fromJson(reader, new TypeToken<List<Ftse100>>() {}.getType());
+		try (Reader reader = new InputStreamReader(Ftse100JsonFileReader.class.getResourceAsStream(jsonFile))){
+			return gson.fromJson(reader, new TypeToken<List<Ftse100>>() {}.getType());
         } catch (IOException e){
         e.printStackTrace();
-    }    return companies;
-}
-// does this need to be a static method? 
-
-// could real the file then set the company as an instance of a class 
-// creating objects of that class with my list of FTse100 then I can carry out these methods on them? i think?
+        return Collections.emptyList();
+        }
+    }
+// does this need to be a static method? yes  
 }
