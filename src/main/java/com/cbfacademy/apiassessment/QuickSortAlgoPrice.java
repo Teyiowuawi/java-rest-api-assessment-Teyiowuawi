8f1521d488467d620e0545457ea1a714da84b677
Bariyat Eyiowuawi <taiwoeyiowuawi@gmail.com>
2023-11-04 10:59:58 +0000

    Implementing HashMap and adding the company Name and company stock price for each company from my Ftse100 list to this hashmap

---
+++ src/main/java/com/cbfacademy/apiassessment/QuickSortAlgoPrice.java
@@ -0,0 +1,25 @@
+package com.cbfacademy.apiassessment;
+
+import java.util.HashMap;
+
+public class QuickSortAlgoPrice {
+    
+    // think about the comparator
+    // array - hashmap two items name and value of price
+    // company name and company price
+    // give sort algo this array
+    // start and end index (1ist object and last object)
+    // pivot point - value of the price for the item at the end index
+    //pivot = array 9  = .getPrice() will be compared to all the other
+
+    HashMap<String, double> companyStockPrice = new HashMap<>();
+
+    // move the values for each company into a hashmap 
+    // then call my quicksort algorithm on this hashmap 
+    // get my list of compay objects
+    // loop through 
+    // for every object 
+    // put (company.getCompanyName(), company.getStockPrice())
+    // return this hashmap 
+    // then I can create my quicksort method 
+}
