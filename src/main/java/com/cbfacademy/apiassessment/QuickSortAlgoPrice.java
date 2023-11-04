6d5239efb61111069253f72d84a7763781c1ce8c
Bariyat Eyiowuawi <taiwoeyiowuawi@gmail.com>
2023-11-04 12:43:33 +0000

    implementing quicksort on list of values after extracting to new array list

--- src/main/java/com/cbfacademy/apiassessment/QuickSortAlgoPrice.java
+++ src/main/java/com/cbfacademy/apiassessment/QuickSortAlgoPrice.java
@@ -1,5 +1,6 @@
 package com.cbfacademy.apiassessment;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -8,27 +9,52 @@ import com.cbfacademy.apiassessment.datamodel.Ftse100;
 public class QuickSortAlgoPrice {
     
    
-    // would i instead place my list of companies inside the constructor rather than reading from the kson file again?
-    public QuickSortAlgoPrice(){}
-    // think about the comparator
-    // array - hashmap two items name and value of price
-    // company name and company price
-    // give sort algo this array
-    // start and end index (1ist object and last object)
-    // pivot point - value of the price for the item at the end index
-    //pivot = array 9  = .getPrice() will be compared to all the other
-
-    String jsonfile = "/ftse100.json";
     
-    private List<Ftse100> companies = Ftse100JsonFileReader.readFtse100JsonFile(jsonfile);
+//     public QuickSortAlgoPrice(){}
+//     // think about the comparator
+//     // array - hashmap two items name and value of price
+//     // company name and company price
+//     // give sort algo this array
+//     // start and end index (1ist object and last object)
+//     // pivot point - value of the price for the item at the end index
+//     //pivot = array 9  = .getPrice() will be compared to all the other
+
+//     String jsonfile = "/ftse100.json";
+    
+//     private List<Ftse100> companies = Ftse100JsonFileReader.readFtse100JsonFile(jsonfile);
+
+//     HashMap<String, Double> companyStockPrice = new HashMap<String, Double>();
+//         for (Ftse100 company: companies){
+//             companyStockPrice.put(company.getCompanyName(), company.getStockPrice());
+//             }
+    
+//     List<Double> extractedPriceValues = new ArrayList<>(companyStockPrice.values());
+    
 
 
-    public HashMap<String, Double> companiesAndStockPrice(){
-        HashMap<String, Double> companyStockPrice = new HashMap<String, Double>();
-        for (Ftse100 company: companies){
-            companyStockPrice.put(company.getCompanyName(), company.getStockPrice());
-            }
-            return companyStockPrice;
-    }
-    // call quicksort on this hashmap 
+//     }
+//     // call quicksort on this hashmap 
+//     // cant be directly done 
+//     // extract the values from the hashmap 
+//     // then call quicksort on it 
+//     // then convert back into my data structure 
+
+//     // alternatively sort list of objects and then place these specific valueus into a hashmap
+//     // pseudo cdoe from class exercise 
+
+// //     public void quickSort(arr, beg, end)
+// //   if (beg < end)
+// //     pivotIndex = partition(arr,beg, end)
+// //     quickSort(arr, beg, pivotIndex)
+// //     quickSort(arr, pivotIndex + 1, end)
+
+// // public void partition(arr, beg, end)
+// //   end = pivotIndex
+// //   pIndex = beg - 1
+// //   for i = beg to end-1
+// //   if arr[i] < pivot
+// //     swap arr[i] and arr[pIndex]
+// //     pIndex++
+// //   swap pivot and arr[pIndex+1]
+// // return pIndex + 1
 }
