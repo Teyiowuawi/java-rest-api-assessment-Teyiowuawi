05322eea9561edadf62964a11419530a0805b4e7
Bariyat Eyiowuawi <taiwoeyiowuawi@gmail.com>
2023-11-05 16:02:44 +0000

    removing unused imports

--- src/main/java/com/cbfacademy/apiassessment/Ftse100JsonFileHandler.java
+++ src/main/java/com/cbfacademy/apiassessment/Ftse100JsonFileHandler.java
@@ -2,13 +2,9 @@ package com.cbfacademy.apiassessment;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.Reader;
 import java.io.Writer;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.List;
 
