8466511105bc79321a72d18ca20eeab2552e5386
Bariyat Eyiowuawi <taiwoeyiowuawi@gmail.com>
2023-11-08 22:01:47 +0000

    final bubble sort implementation

--- src/main/java/com/cbfacademy/apiassessment/bubblesortalgo/BubbleSortAlgo.java
+++ src/main/java/com/cbfacademy/apiassessment/bubblesortalgo/BubbleSortAlgo.java
@@ -8,33 +8,6 @@ import java.util.List;
 import com.cbfacademy.apiassessment.datamodel.Ftse100;
 
 public class BubbleSortAlgo {
-    
-    // public static HashMap<String, Double> bubbleSortStockPrices(HashMap<String, Double> unsortedPricesHashMap){
-    //     List<HashMap.Entry<String, Double>> stockPrices = new ArrayList<>(unsortedPricesHashMap.entrySet());
-        
-    //     boolean swapped = false;
-    //     int prices = stockPrices.size(); 
-
-    //     while(!swapped){
-    //         swapped = true; 
-    //         for (int i = 0; i < prices -1; i++){
-    //             if (stockPrices.get(i).getValue() > stockPrices.get(i + 1).getValue()){
-    //                 HashMap.Entry<String, Double> stockPrice = stockPrices.get(i);
-    //                 stockPrices.set(i, stockPrices.get(i + 1));
-    //                 stockPrices.set(i + 1, stockPrice);
-    //                 swapped = false;
-    //             }
-    //         }
-    //     }
-        
-
-    //     HashMap<String, Double> sortedPrices = new LinkedHashMap<>();
-    
-    //     for (HashMap.Entry<String, Double> stockPrice: stockPrices){
-    //         sortedPrices.put(stockPrice.getKey(), stockPrice.getValue());
-    //     }
-    //     return sortedPrices;
-    // }
 
     public static List<String> bubbleSortStockPrices(List<Ftse100> companies){
         
