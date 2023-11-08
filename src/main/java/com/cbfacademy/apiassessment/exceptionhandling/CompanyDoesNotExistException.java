00c072f86b8ac1c54f9aba383f5830f12c6b9eb1
Bariyat Eyiowuawi <taiwoeyiowuawi@gmail.com>
2023-11-08 22:11:21 +0000

    Removal of comments and un used file

--- src/main/java/com/cbfacademy/apiassessment/exceptionhandling/CompanyDoesNotExistException.java
+++ src/main/java/com/cbfacademy/apiassessment/exceptionhandling/CompanyDoesNotExistException.java
@@ -10,6 +10,4 @@ public class CompanyDoesNotExistException extends RuntimeException{
         super(msg);
         this.message = msg;
     }   
-    // trying to place the status code here to print our what sort of status message it is
-    // but having no luck and not seeing the result in my API tester
 }
