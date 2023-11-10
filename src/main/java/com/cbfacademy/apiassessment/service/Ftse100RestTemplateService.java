0bfeb2308957dcab176bdde3d5ba1d0958e8c8b9
Bariyat Eyiowuawi <taiwoeyiowuawi@gmail.com>
2023-11-10 08:23:33 +0000

    moved URL from rest template to application properties for readability and maintainability

--- src/main/java/com/cbfacademy/apiassessment/service/Ftse100RestTemplateService.java
+++ src/main/java/com/cbfacademy/apiassessment/service/Ftse100RestTemplateService.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -19,22 +20,48 @@ import com.cbfacademy.apiassessment.exceptionhandling.CompanyAlreadyExistsExcept
 
 @Service
 public class Ftse100RestTemplateService {
+   
+    @Value("${rest.url.base}")
+    private String baseUrl;
 
-    RestTemplate restTemplate = new RestTemplate();
+    @Value("${rest.url.companies}") 
+    private String getAllCompaniesURL;
+    
+    @Value("${rest.url.companies}") 
+    private String addNewCompanyURL;
+     
+    @Value("${rest.url.company}") 
+    private String getCompanyURL;
 
-    private static final String getAllCompaniesURL = "http://localhost:8080/companies/all";
-    private static final String addNewCompanyURL = "http://localhost:8080/companies/all";
-    private static final String getCompanyURL = "http://localhost:8080/companies/{tickerSymbol}";
-    private static final String updateCompanyURL = "http://localhost:8080/companies/{tickerSymbol}";
-    private static final String deleteCompanyURL = "http://localhost:8080/companies/{tickerSymbol}";
-    private static final String getCompanyAndStockPriceURL = "http://localhost:8080/companies/stockPrices/{tickerSymbol}";
-    private static final String getCompaniesAndStockPricesURL = "http://localhost:8080/companies/stockPrices/all";
-    private static final String getCompaniesAndMarketCapURL = "http://localhost:8080/companies/marketCapitalization";
-    private static final String getCompaniesAndPriceToEquityURL = "http://localhost:8080/companies/marketCapitalization";
-    private static final String getCompaniesAndPriceToBookURL = "http://localhost:8080/companies/priceToBook";
-    private static final String getCompaniesAndDebtToEquityURL = "http://localhost:8080/companies/debtToEquity";
-    private static final String getCompaniesAndEsgRiskRatingURL = "http://localhost:8080/companies/esg";
-    // look at the next youtube video to add these links to application propeties instead"
+    @Value("${rest.url.company}")
+    private String updateCompanyURL;
+
+    @Value("${rest.url.company}")
+    private String deleteCompanyURL;
+
+    @Value("${rest.url.stockPrices}")
+    private String getCompanyAndStockPriceURL;
+
+    @Value("${rest.url.stockPrices.all}")
+    private String getCompaniesAndStockPricesURL;
+
+    @Value("${rest.url.marketCapitalization}")
+    private String getCompaniesAndMarketCapURL;
+
+    @Value("${rest.url.priceToEquity}")
+    private String getCompaniesAndPriceToEquityURL;
+
+    @Value("${rest.url.priceToBook}")
+    private String getCompaniesAndPriceToBookURL;
+
+    @Value("${rest.url.debtToEquity}")
+    private String getCompaniesAndDebtToEquityURL;
+    
+    @Value("${rest.url.esg}")
+    private String getCompaniesAndEsgRiskRatingURL;
+    
+     RestTemplate restTemplate = new RestTemplate();
+    
     //exception handling - remember to try and then catch these errors!
 
     public ResponseEntity<List<Ftse100>> allCompanies(){
@@ -43,13 +70,13 @@ public class Ftse100RestTemplateService {
 
         HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
         
-        ResponseEntity<List<Ftse100>> response = restTemplate.exchange(getAllCompaniesURL, HttpMethod.GET, entity, new ParameterizedTypeReference<List<Ftse100>>(){});
+        ResponseEntity<List<Ftse100>> response = restTemplate.exchange(baseUrl + getAllCompaniesURL, HttpMethod.GET, entity, new ParameterizedTypeReference<List<Ftse100>>(){});
         return response; 
     }
 
     public ResponseEntity<Ftse100> addCompany(Ftse100 ftse100){
         try{
-            return restTemplate.postForEntity(addNewCompanyURL, ftse100, Ftse100.class);
+            return restTemplate.postForEntity(baseUrl + addNewCompanyURL, ftse100, Ftse100.class);
         } catch (CompanyAlreadyExistsException e){
             throw new CompanyAlreadyExistsException(e.getMessage());
          }
@@ -57,9 +84,9 @@ public class Ftse100RestTemplateService {
 
     public Ftse100 getCompany(String tickerSymbol){
         Map<String, String> param = new HashMap<String,String>();
-        param.put("StockPrice", tickerSymbol);
+        param.put("tickerSymbol", tickerSymbol);
 
-        return restTemplate.getForObject(getCompanyURL, Ftse100.class, param);
+        return restTemplate.getForObject( baseUrl + getCompanyURL, Ftse100.class, param);
     }
 
 
@@ -67,49 +94,51 @@ public class Ftse100RestTemplateService {
         Map<String, String> params = new HashMap<String, String>();
         params.put("tickerSymbol", tickerSymbol);
 
-        restTemplate.put(updateCompanyURL, updatedCompany, params);
+        restTemplate.put(baseUrl + updateCompanyURL, updatedCompany, params);
     }
+    //update should show the new object in the response body
+    // creating a customised response body in springboot
 
     public void deleteCompany(String tickerSymbol) {
         Map<String, String> param = new HashMap<String, String>();
         param.put("tickerSymbol", tickerSymbol);
 
-        restTemplate.delete(deleteCompanyURL, param);
+        restTemplate.delete(baseUrl + deleteCompanyURL, param);
     }
 
     public ResponseEntity<String> getCompanyAndPrice(String tickerSymbol){
-        ResponseEntity<String> response = restTemplate.exchange(getCompanyAndStockPriceURL, HttpMethod.GET, null, String.class, tickerSymbol);
+        ResponseEntity<String> response = restTemplate.exchange(baseUrl + "stockPrices/" + getCompanyAndStockPriceURL, HttpMethod.GET, null, String.class, tickerSymbol);
         return response;
     }
 
     public ResponseEntity<List<String>> getCompaniesAndPrices(){
-        ResponseEntity<List<String>> response = restTemplate.exchange(getCompaniesAndStockPricesURL, HttpMethod.GET, null, new ParameterizedTypeReference<List<String>>(){});
+        ResponseEntity<List<String>> response = restTemplate.exchange(baseUrl + "stockPrices/" + getCompaniesAndStockPricesURL, HttpMethod.GET, null, new ParameterizedTypeReference<List<String>>(){});
         return response;
     }
 
 
     public ResponseEntity<List<String>> getCompaniesAndMarketCap(){
-        ResponseEntity<List<String>> response = restTemplate.exchange(getCompaniesAndMarketCapURL, HttpMethod.GET, null, new ParameterizedTypeReference<List<String>>(){});
+        ResponseEntity<List<String>> response = restTemplate.exchange(baseUrl + getCompaniesAndMarketCapURL, HttpMethod.GET, null, new ParameterizedTypeReference<List<String>>(){});
         return response;
     }
 
     public ResponseEntity<List<String>> getCompaniesAndPriceToEquity(){
-        ResponseEntity<List<String>> response = restTemplate.exchange(getCompaniesAndPriceToEquityURL, HttpMethod.GET, null, new ParameterizedTypeReference<List<String>>(){});
+        ResponseEntity<List<String>> response = restTemplate.exchange(baseUrl + getCompaniesAndPriceToEquityURL, HttpMethod.GET, null, new ParameterizedTypeReference<List<String>>(){});
         return response;
     }
 
     public ResponseEntity<List<String>>getCompaniesAndPriceToBook(){
-        ResponseEntity<List<String>> response = restTemplate.exchange(getCompaniesAndPriceToBookURL, HttpMethod.GET, null, new ParameterizedTypeReference<List<String>>(){});
+        ResponseEntity<List<String>> response = restTemplate.exchange(baseUrl + getCompaniesAndPriceToBookURL, HttpMethod.GET, null, new ParameterizedTypeReference<List<String>>(){});
         return response;
     }
 
     public ResponseEntity<List<String>> getCompaniesAndDebtToEquity(){
-        ResponseEntity<List<String>> response = restTemplate.exchange(getCompaniesAndDebtToEquityURL, HttpMethod.GET, null, new ParameterizedTypeReference<List<String>>(){});
+        ResponseEntity<List<String>> response = restTemplate.exchange(baseUrl + getCompaniesAndDebtToEquityURL, HttpMethod.GET, null, new ParameterizedTypeReference<List<String>>(){});
         return response;
     }
 
     public ResponseEntity<List<String>>getCompaniesAndEsgRiskRating(){
-        ResponseEntity<List<String>> response = restTemplate.exchange(getCompaniesAndEsgRiskRatingURL, HttpMethod.GET, null, new ParameterizedTypeReference<List<String>>(){});
+        ResponseEntity<List<String>> response = restTemplate.exchange(baseUrl + getCompaniesAndEsgRiskRatingURL, HttpMethod.GET, null, new ParameterizedTypeReference<List<String>>(){});
         return response;
     }
 
