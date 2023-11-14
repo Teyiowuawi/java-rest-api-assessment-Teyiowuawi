9e0ee86b60adcd294b714178185fe12896304f41
Bariyat Eyiowuawi <taiwoeyiowuawi@gmail.com>
2023-11-14 10:14:54 +0000

    changes to File writing test with Market cap value. Have changed data type for market cap to long instead

--- src/test/java/com/cbfacademy/apiassessment/AppTests.java
+++ src/test/java/com/cbfacademy/apiassessment/AppTests.java
@@ -54,14 +54,6 @@ class AppTests {
 		assertEquals("Hello World", response.getBody());
 	}
 
-	@Test
-	@Description("/greeting endpoint returns expected response for specified name parameter")
-	public void greeting_ExpectedResponseWithNameParam() {
-		ResponseEntity<String> response = restTemplate.getForEntity(base.toString() + "?name=John", String.class);
-
-		assertEquals(200, response.getStatusCode().value());
-		assertEquals("Hello John", response.getBody());
-	}
 
 	@Test
 	@Description("Reading from json file and returning list of objects")
@@ -80,20 +72,17 @@ class AppTests {
 
 
 	@Test
-	@Description("Writing from java objects back to json file")
+	@Description("Writing to json file and comparing output")
 	public void writeToJsonFile() throws IOException {
 
 		final String jsonTestFile = "src/test/resources/ftseTest.json";
-		
-		// if !(jsonTestFile.exists()){
-			// if file doesnt exist throw exception here 
 
 			List<Ftse100> expectedCompanies = new ArrayList<>(){
 			{
 				add (new Ftse100("Endeavour Plc", "EDV", "Precious Metals and Mining", 1678.0, new BigInteger("4333000000"), -17.22, 1.52, 27.23, 23.09));
 				add (new Ftse100("ConvaTec Group Plc", "CTEC", "Medical Equipment and Services", 201.08, new BigInteger("4554000000"), 67.67, 3.11, 90.12, 16.6));
 				add (new Ftse100("BAE Systems Plc", "BA", "Aerospace and Defense", 1086.0, new BigInteger("32770000000"), 16.85, 3.04, 61.08, 31.6));
-				add (new Ftse100("Unite Group Plc", "UTG", "Real Estate", 854.5, new BigInteger("39840000000"), 26.5, 0.99, 35.73, 15.5));
+				add (new Ftse100("Unite Group Plc", "UTG", "Real Estate", 854.5, new BigInteger("3984000000"), 26.5, 0.99, 35.73, 15.5));
 			}
 		};
 
@@ -104,11 +93,11 @@ class AppTests {
 
 		assertTrue(new File(jsonTestFile).exists());
 		assertEquals(expectedCompanies, actualCompanies);
-
-				// doesnt work currently - keep springting null
-		// my have to compare them directly due to data structure 
 		
 		}
+		// getting a return of null for this test 
+				// if !(jsonTestFile.exists()){
+			// if file doesnt exist throw exception here 
 
 	@Test 
 	@Description("Returns a list of company names, ticker symbols and stock prices")
@@ -134,47 +123,14 @@ class AppTests {
 		};
 
 		assertEquals(sortedPricesExpectedOutput, BubbleSortAlgo.bubbleSortStockPrices(companies));
-			}
-		
-
+	}
 
 }
 
-
-
 // 	// TESTS I WANT TO WRITE 
-// 	// reading and writing from a file 
-
-	// test my algorithm class 
-	// probabaly my rest template also 
-	// test that my exceptions work too
-
-
-	//  @Test
-    // public void testBubbleSortCompaniesByStockPrice() {
-//  Create a dummy array list perhaps?
-// Or i could set up a list before hand? to create a list of companies and use the values from it?
-// hm give it a think, it would be a put long 
-// instantiate a list of objects with a few metrics inside 
-// void whenSortedWithBubbleSort_thenGetSortedArray() {
-//     Integer[] array = { 2, 1, 4, 6, 3, 5 };
-//     Integer[] sortedArray = { 1, 2, 3, 4, 5, 6 };
-//     BubbleSort bubbleSort = new BubbleSort();
-//     bubbleSort.bubbleSort(array);
-    // assertArrayEquals(array, sortedArray);
-	// example way to test if bubble sort algorithm works 
-
-// asserts and exception handling - remember to throw them
-
-		// List<Ftse100> expectedCompaniesList = new ArrayList<>(){
-		// 	{
-		// 		add (new Ftse100("Endeavour Plc", "EDV", "Precious Metals and Mining", 1678.0, new BigInteger("4333000000"), -17.22, 1.52, 27.23, 23.09));
-		// 		add (new Ftse100("ConvaTec Group Plc", "CTEC", "Medical Equipment and Services", 201.08, new BigInteger("4554000000"), 67.67, 3.11, 90.12, 16.6));
-		// 		add (new Ftse100("BAE Systems Plc", "BA", "Aerospace and Defense", 1086.0, new BigInteger("32770000000"), 16.85, 3.04, 61.08, 31.6));
-		// 		add (new Ftse100("Unite Group Plc", "UTG", "Real Estate", 854.5, new BigInteger("39840000000"), 26.5, 0.99, 35.73, 15.5));
-		// 	}
-			
-		// }; 
+// writing from a file 
+// rest template also 
+// exception handling in file reading 
 
 	// @Test
 	// @DisplayName("get specific FTSE 100 company")
@@ -187,5 +143,5 @@ class AppTests {
 
 // 	// @Test
 // 	// @DisplayName("update information of a FTSE 100 company")
-// 	// remeber to test that exceptions are also thrown!
+
 
