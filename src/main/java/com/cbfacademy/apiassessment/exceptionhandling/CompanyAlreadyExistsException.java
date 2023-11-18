1df024ef9ea668299225890fc08c93214dea9a8e
Bariyat Eyiowuawi <taiwoeyiowuawi@gmail.com>
2023-11-18 12:53:04 +0000

    created body response for excpetions when inappropriate request is made, adjusted company already exists exception to match format of company not found exception

--- src/main/java/com/cbfacademy/apiassessment/exceptionhandling/CompanyAlreadyExistsException.java
+++ src/main/java/com/cbfacademy/apiassessment/exceptionhandling/CompanyAlreadyExistsException.java
@@ -4,13 +4,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
 public class CompanyAlreadyExistsException extends ResponseStatusException{
-   
-    private String message; 
 
     public CompanyAlreadyExistsException(HttpStatus status, String message){
-        super();
-        // remove the messaeg aspect 
-        // add the status code
+        super(status, message);
+
     }
 }
 
--- src/main/java/com/cbfacademy/apiassessment/exceptionhandling/ErrorResponse.java
+++ src/main/java/com/cbfacademy/apiassessment/exceptionhandling/ErrorResponse.java
@@ -2,12 +2,32 @@ package com.cbfacademy.apiassessment.exceptionhandling;
 
 public class ErrorResponse {
 
+    private int statusCode;
     private String message; 
 
-    public ErrorResponse(){}
-
-    public ErrorResponse(String message){
-        super();
+    public ErrorResponse(int statusCode, String message){
+        this.statusCode = statusCode;
         this.message = message;
     }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+        public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    
+
+    
+
 }
