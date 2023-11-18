ba7d1645f51c7d152523eb578de303d74e92abbd
Bariyat Eyiowuawi <taiwoeyiowuawi@gmail.com>
2023-11-18 12:19:00 +0000

    creating response handler to deal with specific exceptions

---
+++ src/main/java/com/cbfacademy/apiassessment/exceptionhandling/ErrorResponse.java
@@ -0,0 +1,13 @@
+package com.cbfacademy.apiassessment.exceptionhandling;
+
+public class ErrorResponse {
+
+    private String message; 
+
+    public ErrorResponse(){}
+
+    public ErrorResponse(String message){
+        super();
+        this.message = message;
+    }
+}
